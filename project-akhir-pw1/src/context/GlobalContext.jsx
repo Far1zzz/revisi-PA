6fe505ac6b7ef4dbde12f302078867c171b87817
farizz <fairizwalker@gmail.com>
2022-12-23 21:20:23 +0700

    update

---
+++ project-akhir-pw1/src/context/GlobalContext.jsx
@@ -0,0 +1,7 @@
+import React, { createContext, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export const GlobalContext = createContext();
+
+export const GlobalProvider = (props) => {};
---
+++ project-akhir-pw1/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
